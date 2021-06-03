8f84ba8ab695f9c2956f24a7384b66d43e1b8e63
xiaohong <chenxiaohong@cmcm.com>
2021-06-03 19:09:02 +0800

    删除冗余错误代码

--- app/src/main/java/com/linkv/linkvrtmdemo/MainActivity.java
+++ app/src/main/java/com/linkv/linkvrtmdemo/MainActivity.java
@@ -83,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
     private void initRTMEngine() {
         mEngine = RtmEngineManager.createEngine(getApplication());
         mEngine.setIMAuthEventListener(imAuthEventListener);
-        mEngine.setGlobalReceiveMessageListener(IMBridger.IMReceiveMessageListener receiveMessageListener);
+        mEngine.setGlobalReceiveMessageListener(receiveMessageListener);
 
     }
 
